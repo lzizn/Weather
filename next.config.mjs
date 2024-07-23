feab9be7814396cb530c8a4b63f6f80bac9cb8d7
Laian <braumlaian@gmail.com>
2024-07-23 16:14:16 -0300

    fix: add envs to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_OPENWEATHER_KEY: process.env.NEXT_PUBLIC_OPENWEATHER_KEY,
+    NEXT_PUBLIC_GEOCODING_KEY: process.env.NEXT_PUBLIC_GEOCODING_KEY,
+  },
+};
 
 export default nextConfig;
 

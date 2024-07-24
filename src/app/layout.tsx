22d69c758aef2bef9fb2eee84f88c41ff346eef9
Laian <braumlaian@gmail.com>
2024-07-24 09:54:35 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,11 @@ import type { Metadata } from 'next';
 import { StyledComponentsRegistry } from '@/lib/registry';
 
 export const metadata: Metadata = {
-  title: 'Web Weather Challenge',
+  title: 'weather',
   description: `This is a test to get to know one's front-end development skills, the goal here is to build a weather web app, with a responsive and intuitive design, and most importantly, good code practices.`,
+  icons: {
+    icon: '/icon.png',
+  },
 };
 
 export default function RootLayout({
